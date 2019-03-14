f2ace00717d16ade057bddd6267ffa68e2dc2485
Alex Mages <alemages@gmail.com>
2019-03-13 21:31:49 -0500

    Added styling to trace

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 
 # Config file for MySQL Connector
 config = {
-    'user': 'root',
-    'password': 'lptech',
-    'host': '127.0.0.1',
-    'database': 'test',
-    'raise_on_warnings': True
+    "user": "root",
+    "password": "lptech",
+    "host": "127.0.0.1",
+    "database": "test",
+    "raise_on_warnings": True
 }
 
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
 
 def twos_comp (val):
     comp_eval = int(val, 16) - pow(2, 32)
@@ -17,24 +19,34 @@ def twos_comp (val):
     return comp_eval
 
 
-cnx = mysql.connector.connect(**config)
-cursor = cnx.cursor()
+def main():
 
-query = ("SELECT trace_data FROM test")
+    query = ("SELECT * FROM test")
+    cursor.execute(query)
+           
 
-cursor.execute(query)
+    for entry in cursor:
+        blob_array = []
+        blob = entry[1].hex()
+        for i in range(0, len(blob), 8):
+            two_eval = twos_comp(blob[i:i+8])
+            blob_array.append(two_eval/1000)
 
-for entry in cursor:
-    blob_array = []
-    blob = entry[0].hex()
-    for i in range(0, len(blob), 8):
-        two_eval = twos_comp(blob[i:i+8])
-        blob_array.append(two_eval/1000)
-    plt.plot(blob_array)
-    plt.draw()
-    plt.pause(.1)
-    plt.clf()
+        plt.style.use("dark_background")
+        plt.plot(blob_array, 'y', linewidth=1)
+        plt.xlabel("MHz")
+        plt.xlim(100, 500)
+        plt.ylabel("dBm")
+        plt.ylim(-60, -30)
+        plt.grid(color="gray", linestyle="-", linewidth=0.5)
+        plt.text(250, -59.1, entry[2])
+        plt.text(425, -33, "LPT Example", color="yellow")
+        plt.draw()
+        plt.pause(.1)
+        plt.clf()
 
 
-cnx.close()
-
+if __name__ == "__main__":
+    while True:
+        main()
+    cnx.close()
