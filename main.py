1acae0bb7570049aa34590c28705a5e8723fbeee
Alex Mages <alemages@gmail.com>
2019-03-13 21:57:31 -0500

    Added configurable center frequency and span (X-axis)

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ config = {
 cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor()
 
-def twos_comp (val):
+
+# Function to convert Hex to signed int
+def twos_comp(val):
     comp_eval = int(val, 16) - pow(2, 32)
     comp_eval = int(hex(comp_eval), 16)
     return comp_eval
@@ -23,8 +25,18 @@ def main():
 
     query = ("SELECT * FROM test")
     cursor.execute(query)
-           
 
+    # Creates X axis, change cen_freq and span to match analyzers settings
+    trace_array = []
+    cen_freq = 1000
+    span = 1000
+    hal_span = float(span / 2)
+    inc_span = float("{0:.2f}".format(span / 601))
+    freq_star = cen_freq - hal_span
+    for i in range(601):
+        trace_array.append(freq_star + i * inc_span)
+
+    # For loop to pull blob, convert to dBm, and then graph
     for entry in cursor:
         blob_array = []
         blob = entry[1].hex()
@@ -32,21 +44,22 @@ def main():
             two_eval = twos_comp(blob[i:i+8])
             blob_array.append(two_eval/1000)
 
+        # Styling for plot
         plt.style.use("dark_background")
-        plt.plot(blob_array, 'y', linewidth=1)
+        plt.plot(trace_array, blob_array, 'y', linewidth=1)
         plt.xlabel("MHz")
-        plt.xlim(100, 500)
+        plt.xlim(cen_freq-hal_span, cen_freq+hal_span)
         plt.ylabel("dBm")
         plt.ylim(-60, -30)
         plt.grid(color="gray", linestyle="-", linewidth=0.5)
-        plt.text(250, -59.1, entry[2])
-        plt.text(425, -33, "LPT Example", color="yellow")
+        plt.text(cen_freq - 85 * inc_span, -59.1, entry[2])
+        plt.text(cen_freq + 185 * inc_span, -33, "LPT Example", color="yellow")
         plt.draw()
-        plt.pause(.1)
+        # Time replay speed
+        plt.pause(1)
         plt.clf()
 
 
 if __name__ == "__main__":
     while True:
         main()
-    cnx.close()
